eb7adcea06966187d967cd2687e7852db1c4d23a
Ali Khali <ak126pk@gmail.com>
2025-01-14 00:04:21 +0500

    Add Project in portfolio

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,11 +1,10 @@
 import React from "react";
 import prjct2 from "../Assets/project2.jpg";
 import prjct3 from "../Assets/project3.png";
-
+import prjct4 from "../Assets/project4.png";
 
 // Project data
 const projects = [
-  
   {
     id: 1,
     title: "Medicine Recommendation System",
@@ -13,7 +12,8 @@ const projects = [
       "Developed a machine learning system for disease prediction based on symptoms. Utilized Python Flask for the backend and React.js for the frontend.",
     image: prjct2,
     demoLink: "https://medicine-recommendation-system-frontend.vercel.app/",
-    codeLink: "https://github.com/AliKhalil28/Medicine_Recommendation_System_Frontend",
+    codeLink:
+      "https://github.com/AliKhalil28/Medicine_Recommendation_System_Frontend",
   },
   {
     id: 2,
@@ -24,53 +24,55 @@ const projects = [
     demoLink: "https://www.codewithali.info/",
     codeLink: "hhttps://github.com/AliKhalil28/portfolio",
   },
-  
-    
+  {
+    id: 3,
+    title: "Random User API using React Js",
+    description:
+      "A React app that fetches and displays user profiles from the Random User API with pagination. Built using React hooks, and Tailwind CSS and responsive user interface.",
+    image: prjct4,
+    demoLink:
+      "https://randam-user-api-react-3cq755fls-ali-khalils-projects-3784c504.vercel.app/",
+    codeLink: "https://github.com/AliKhalil28/RandamUserAPIReact",
+  },
 ];
 
 const Project = () => {
-
   return (
-    
-
     <section className="projects" id="prosec">
-    <h1>Projects</h1>
-    <div className="card-body">
-    <div className="card">
-  {projects.map((project, index) => (
-    <div key={index} className="project-item">
-      <img src={project.image} alt={`${project.title} screenshot`} />
-      <h3>{project.title}</h3>
-      <p>{project.description}</p>
-      <div className="pro-bottom">
-        <div className="pro-bottom-left">
-          <a
-            href={project.demoLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-dark"
-          >
-            <i className="fa fa-eye" aria-hidden="true"></i>
-          </a>
-        </div>
-        <div className="pro-bottom-right">
-          <a
-            href={project.codeLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="btn btn-dark"
-          >
-            <i className="fa-brands fa-github" aria-hidden="true"></i>
-          </a>
+      <h1>Projects</h1>
+      <div className="card-body">
+        <div className="card">
+          {projects.map((project, index) => (
+            <div key={index} className="project-item">
+              <img src={project.image} alt={`${project.title} screenshot`} />
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <div className="pro-bottom">
+                <div className="pro-bottom-left">
+                  <a
+                    href={project.demoLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-dark"
+                  >
+                    <i className="fa fa-eye" aria-hidden="true"></i>
+                  </a>
+                </div>
+                <div className="pro-bottom-right">
+                  <a
+                    href={project.codeLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-dark"
+                  >
+                    <i className="fa-brands fa-github" aria-hidden="true"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    </div>
-  ))}
-</div>
-
-      
-    </div>
-
     </section>
   );
 };
